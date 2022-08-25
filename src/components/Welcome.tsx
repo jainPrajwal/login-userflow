dbd8e542e86fab4938e36b07114f78bd7e239211
unknown <jainprajwal123@gmail.com>
2022-08-25 22:37:03 +0530

    refactor: welcome component and handlers

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,36 +1,163 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { AppData } from "../constants/data.types";
 import { useData } from "../hooks/useData";
 import { useFormCheck } from "../hooks/useFormCheck";
+
+
+const handlerFactory = {
+    onFocusHandler: ({ setFormState, field }: {
+        setFormState: React.Dispatch<React.SetStateAction<{
+            fullName: {
+                isTouched: boolean;
+                errorMessage: string;
+                isValid: boolean;
+            };
+            displayName: {
+                isTouched: boolean;
+                errorMessage: string;
+                isValid: boolean;
+            };
+        }>>,
+        field: `fullName` | `displayName`
+    }) => {
+
+
+        setFormState(prevState => ({
+            ...prevState, [field]: {
+                ...prevState[field],
+                isTouched: true,
+
+            }
+        }))
+
+
+
+    },
+
+
+    onBlurHandler: ({ e, setFormState, setAppData, field }: {
+        e: React.FocusEvent<HTMLInputElement, Element>,
+        setFormState: React.Dispatch<React.SetStateAction<{
+            fullName: {
+                isTouched: boolean;
+                errorMessage: string;
+                isValid: boolean;
+            };
+            displayName: {
+                isTouched: boolean;
+                errorMessage: string;
+                isValid: boolean;
+            };
+        }>>
+        setAppData: React.Dispatch<React.SetStateAction<AppData>>
+        field: `fullName` | `displayName`
+    }) => {
+
+        if (e.target.value.length <= 0) {
+            setFormState(prevState => {
+                return {
+                    ...prevState,
+                    [field]: {
+                        ...prevState[field],
+                        isValid: false,
+                        errorMessage: `Name cannot be blank`
+                    }
+                }
+            })
+        } else {
+
+            if (e.target.value.match(/^[a-zA-Z_]+$/)) {
+                setAppData(prevState => {
+                    return {
+                        ...prevState,
+                        validations: {
+                            isFormValid: true,
+
+                        }
+                    }
+                })
+
+                setFormState(prevState => {
+                    return {
+                        ...prevState,
+                        [field]: {
+                            ...prevState[field],
+                            isValid: true,
+                            errorMessage: ``
+                        }
+                    }
+                })
+            } else {
+                setFormState(prevState => {
+                    return {
+                        ...prevState,
+                        [field]: {
+                            ...prevState[field],
+                            isValid: false,
+                            errorMessage: `Name cannot contain anything except letters`
+                        }
+                    }
+                })
+            }
+        }
+
+
+
+
+    },
+
+
+    onChangeHandler: ({ e, setAppData, field }: { e: React.ChangeEvent<HTMLInputElement>, setAppData: React.Dispatch<React.SetStateAction<AppData>>, field: string }) => {
+        setAppData(prevState => {
+            return {
+                ...prevState,
+                userData: {
+                    ...prevState.userData,
+                    [field]: e.target.value
+                }
+            }
+        })
+
+    }
+}
+
+
+
+const initialFormState = {
+    fullName: {
+        isTouched: false,
+        errorMessage: ``,
+        isValid: false,
+
+    },
+    displayName: {
+        isTouched: false,
+        errorMessage: ``,
+        isValid: false,
+    }
+}
+
+
 
 export const Welcome = () => {
     const { appData: { userData: { displayName, fullName } }, setAppData } = useData();
 
+
+    const { onBlurHandler, onChangeHandler, onFocusHandler } = handlerFactory;
+
     useFormCheck();
 
 
-    const [formState, setFormState] = useState({
-        fullName: {
-            isTouched: false,
-            errorMessage: ``,
-            isValid: false,
 
-        },
-        displayName: {
-            isTouched: false,
-            errorMessage: ``,
-            isValid: false,
-        }
-    })
+    const [formState, setFormState] = useState(initialFormState)
     return <><section className='d-flex f-direction-col ai-center w-100'>
 
 
         <h1 className='header-primary'>Welcome! First things first...</h1>
         <p className='text-secondary text-center py-md'>You can always change them later.</p>
-        <div className=''>
 
-        </div>
         <div className='w-80'>
-            <form action="">
+            <form >
                 <div className='d-flex f-direction-col p-md'>
                     <label htmlFor="workspace-name" className='my-sm text-bold'>
                         Full Name
@@ -41,88 +168,11 @@ export const Welcome = () => {
                             outline: formState.fullName.errorMessage ? `1px solid var(--flow-error)` : ``
                         }}
 
-                        onFocus={() => {
+                        onFocus={() => { onFocusHandler({ setFormState, field: `fullName` }) }}
 
+                        onChange={(e) => { onChangeHandler({ e, setAppData, field: `fullName` }) }}
 
-                            setFormState(prevState => ({
-                                ...prevState, fullName: {
-                                    ...prevState.fullName,
-                                    isTouched: true,
-
-                                }
-                            }))
-
-
-
-                        }}
-
-                        onChange={(e) => {
-                            setAppData(prevState => {
-                                return {
-                                    ...prevState,
-                                    userData: {
-                                        ...prevState.userData,
-                                        fullName: e.target.value
-                                    }
-                                }
-                            })
-
-                        }}
-
-                        onBlur={(e) => {
-
-                            if (e.target.value.length <= 0) {
-                                setFormState(prevState => {
-                                    return {
-                                        ...prevState,
-                                        fullName: {
-                                            ...prevState.fullName,
-                                            isValid: false,
-                                            errorMessage: `Name cannot be blank`
-                                        }
-                                    }
-                                })
-                            } else {
-
-                                if (e.target.value.match(/^[a-zA-Z_]+$/)) {
-                                    setAppData(prevState => {
-                                        return {
-                                            ...prevState,
-                                            validations: {
-                                                isFormValid: true,
-
-                                            }
-                                        }
-                                    })
-
-                                    setFormState(prevState => {
-                                        return {
-                                            ...prevState,
-                                            fullName: {
-                                                ...prevState.fullName,
-                                                isValid: true,
-                                                errorMessage: ``
-                                            }
-                                        }
-                                    })
-                                } else {
-                                    setFormState(prevState => {
-                                        return {
-                                            ...prevState,
-                                            fullName: {
-                                                ...prevState.fullName,
-                                                isValid: false,
-                                                errorMessage: `Name cannot contain anything except letters`
-                                            }
-                                        }
-                                    })
-                                }
-                            }
-
-
-
-
-                        }}
+                        onBlur={(e) => { onBlurHandler({ e, setFormState, setAppData, field: `fullName` }) }}
                         id='workspace-name'
                         className='input p-lg'
                         placeholder='Eden'
@@ -146,90 +196,15 @@ export const Welcome = () => {
                         }}
                         value={displayName || ``}
 
-                        onFocus={() => {
 
 
-                            setFormState(prevState => ({
-                                ...prevState, displayName: {
-                                    ...prevState.displayName,
-                                    isTouched: true,
+                        onFocus={() => { onFocusHandler({ setFormState, field: `displayName` }) }}
 
-                                }
-                            }))
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => { onChangeHandler({ e, setAppData, field: `displayName` }) }}
 
+                        onBlur={(e: React.FocusEvent<HTMLInputElement, Element>) => { onBlurHandler({ e, setFormState, setAppData, field: `displayName` }) }}
 
-
-                        }}
-
-                        onChange={(e) => {
-                            setAppData(prevState => {
-                                return {
-                                    ...prevState,
-                                    userData: {
-                                        ...prevState.userData,
-                                        displayName: e.target.value
-                                    }
-                                }
-                            })
-
-                        }}
-
-                        onBlur={(e) => {
-
-                            if (e.target.value.length <= 0) {
-                                setFormState(prevState => {
-                                    return {
-                                        ...prevState,
-                                        displayName: {
-                                            ...prevState.displayName,
-                                            isValid: false,
-                                            errorMessage: `Name cannot be blank`
-                                        }
-                                    }
-                                })
-                            } else {
-
-                                if (e.target.value.match(/^[a-zA-Z_]+$/)) {
-                                    setAppData(prevState => {
-                                        return {
-                                            ...prevState,
-                                            validations: {
-                                                isFormValid: true,
-
-                                            }
-                                        }
-                                    })
-
-                                    setFormState(prevState => {
-                                        return {
-                                            ...prevState,
-                                            displayName: {
-                                                ...prevState.displayName,
-                                                isValid: true,
-                                                errorMessage: ``
-                                            }
-                                        }
-                                    })
-                                } else {
-                                    setFormState(prevState => {
-                                        return {
-                                            ...prevState,
-                                            displayName: {
-                                                ...prevState.displayName,
-                                                isValid: false,
-                                                errorMessage: `Name cannot contain numbers`
-                                            }
-                                        }
-                                    })
-                                }
-                            }
-
-
-
-
-                        }}
-
-
+                        
                         id='workspace-name'
                         className='input p-lg w-100'
                         placeholder='Example'
